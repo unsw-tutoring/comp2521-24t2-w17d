9cc44ec22c7b3b789fcb6bc073588a85e807aab4
Josh <joshconstantinelim@gmail.com>
2024-07-17 18:39:06 +1000

    wk8

--- wk8/headerGuards/adt2.h
+++ wk8/headerGuards/adt2.h
@@ -1,11 +1,8 @@
-#ifndef ADT2_H
-#define ADT2_H
+// #ifndef ADT2_H
+// #define ADT2_H
 
 #include "adt1.h"
 
 int adt2Function(void);
 
-#endif
-
-
-
+// #endif
--- wk8/reachable.c
+++ wk8/reachable.c
@@ -5,7 +5,20 @@
 #include "Graph.h"
 #include "Set.h"
 
+static void doReachable(Graph g, Vertex v, Set visited) {
+    SetInsert(visited, v);
+
+    for (int w = 0; w < GraphNumVertices(g); w++) {
+        if (GraphIsAdjacent(g, v, w) && !SetContains(visited, w)) {
+            doReachable(g, w, visited);
+        }
+    }
+}
+
 Set reachable(Graph g, Vertex src) {
-    // TODO
-    return NULL;
+    Set visited = SetNew();
+
+    doReachable(g, src, visited);
+
+    return visited;
 }
